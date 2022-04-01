948400bf5c41a74bfe76dbab924d5b2706893ec2
Carlos <krloslao90@gmail.com>
2022-04-01 16:21:33 -0400

    update repo

---
+++ Javascript/React Learning Path (LinkedIn Learning)/src/content/01CreatingComponents.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function CreatingComponents() {
+  return <div>
+      This is a quick way to create and export a component !
+  </div>;
+}
+
+export default CreatingComponents;
+
