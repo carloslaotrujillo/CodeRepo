c46bee81b2e7f9a9db0e1dd503979f448d1869f6
Carlos Trujillo <krloslao90@gmail.com>
2022-04-02 13:35:07 -0400

    finishing LL section

--- Algo & DT/Data Structires And Algorithms - JS (Scott Barret)/src/03. Data Stuctures/01. Linked List/01. LL Introduction.js
+++ Algo & DT/Data Structires And Algorithms - JS (Scott Barret)/src/03. Data Stuctures/01. Linked List/01. LL Introduction.js
@@ -4,4 +4,4 @@
 
 // The have a variable that points to the "head" item and other called "tail" that points to the last item
 
-// Each item on the list points to the next, and the last one points to "null" (frease "null terminated list")
+// Each item on the list points to the next, and the last one points to "null" (frase "null terminated list")
