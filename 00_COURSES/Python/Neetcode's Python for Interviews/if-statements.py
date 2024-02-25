e5b6a17bde7dadfb1449829456562c4299bb5d24
Charles La O <krloslao90@gmail.com>
2024-02-25 14:20:50 -0500

    WIP

---
+++ 00_COURSES/Python/Neetcode's Python for Interviews/if-statements.py
@@ -0,0 +1,17 @@
+# If statements don't need parentheses 
+# or curly braces.
+n = 1
+if n > 2:
+    n -= 1
+elif n == 2:
+    n *= 2
+else:
+    n += 2
+
+# Parentheses needed for multi-line conditions.
+# and = &&
+# or  = ||
+n, m = 1, 2
+if ((n > 2 and 
+    n != m) or n == m):
+    n += 1
---
+++ 00_COURSES/Python/Neetcode's Python for Interviews/loops.py
@@ -0,0 +1,16 @@
+n = 5
+while n < 5:
+    print(n)
+    n += 1
+
+# Looping from i = 0 to i = 4
+for i in range(5):
+    print(i)
+
+# Looping from i = 2 to i = 5
+for i in range(2, 6):
+    print(i)
+
+# Looping from i = 5 to i = 2
+for i in range(5, 1, -1):
+    print(i)
